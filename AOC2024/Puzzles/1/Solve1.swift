85760ee30d457a895bcf332ae23e98e00e583191
Richard Pineo <Richard.E.Pineo@ge.com>
2024-12-01 18:44:24 -0800

    solve 1a

---
+++ AOC2024/Puzzles/1/Solve1.swift
@@ -0,0 +1,8 @@
+//
+//  Solve1.swift
+//  AOC2024
+//
+//  Created by Richard Pineo on 12/1/24.
+//
+
+import Foundation
---
+++ AOC2024/Puzzles2024.swift
@@ -0,0 +1,8 @@
+//
+//  Puzzles2024.swift
+//  AOC2024
+//
+//  Created by Richard Pineo on 12/1/24.
+//
+
+import Foundation
