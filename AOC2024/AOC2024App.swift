576231191edbb69e7a631eae107ec7abff1b22fb
Richard Pineo <Richard.E.Pineo@ge.com>
2024-12-01 17:37:00 -0800

    Initial Commit

---
+++ AOC2024/AOC2024App.swift
@@ -0,0 +1,17 @@
+//
+//  AOC2024App.swift
+//  AOC2024
+//
+//  Created by Richard Pineo on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct AOC2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AOC2024Tests/AOC2024Tests.swift
@@ -0,0 +1,17 @@
+//
+//  AOC2024Tests.swift
+//  AOC2024Tests
+//
+//  Created by Richard Pineo on 12/1/24.
+//
+
+import Testing
+@testable import AOC2024
+
+struct AOC2024Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
