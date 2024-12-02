e9d36c48672a1fd083fb7c51a83970de52c70044
Richard Pineo <Richard.E.Pineo@ge.com>
2024-12-01 18:49:02 -0800

    1b

--- AOC2024/Puzzles/1/Solve1.swift
+++ AOC2024/Puzzles/1/Solve1.swift
@@ -8,11 +8,11 @@ class Solve1: PuzzleSolver {
 	}
 
 	func solveBExamples() -> Bool {
-		solveB("Example1") == 0
+		solveB("Example1") == 31
 	}
 
-	var answerA = ""
-	var answerB = ""
+	var answerA = "1651298"
+	var answerB = "21306195"
 
 	func solveA() -> String {
 		solveA("Input1").description
@@ -42,7 +42,22 @@ class Solve1: PuzzleSolver {
 	}
 
 	func solveB(_ fileName: String) -> Int {
-		let lines = FileHelper.load(fileName)!.filter { !$0.isEmpty }
-		return lines.count
+		let lines = FileHelper.loadAndTokenize(fileName).filter { !$0.isEmpty }
+		var lhs: [Int] = []
+		var rhs: [Int] = []
+		for line in lines {
+			lhs.append(Int(line[0])!)
+			rhs.append(Int(line[3])!)
+		}
+
+		func rhsOccurrence(_ val: Int) -> Int {
+			rhs.count { $0 == val }
+		}
+		
+		var score = 0
+		for val in lhs {
+			score += val * rhsOccurrence(val)
+		}
+		return score
 	}
 }
