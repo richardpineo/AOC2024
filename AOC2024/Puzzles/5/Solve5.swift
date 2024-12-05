e3d61841141e631b260bf5b860fef9b34b62436e
Richard Pineo <Richard.E.Pineo@ge.com>
2024-12-04 21:30:38 -0800

    5a

--- AOC2024/Puzzles/5/Solve5.swift
+++ AOC2024/Puzzles/5/Solve5.swift
@@ -11,9 +11,34 @@ class Solve5: PuzzleSolver {
 		solveB("Example5") == 0
 	}
 
-	var answerA = ""
+	var answerA = "4924"
 	var answerB = ""
 
+	struct Ordering {
+		var first: Int
+		var second: Int
+	}
+	
+	class Job {
+		var orderings: [Ordering] = []
+		var pages: [[Int]] = []
+	}
+	
+	func load(_ fileName: String) -> Job {
+		let lines = FileHelper.load(fileName)!
+		let pageBreak = lines.firstIndex { $0.isEmpty }!
+		let job: Job = .init()
+		for index in 0 ..< pageBreak {
+			let ordering = lines[index].split(separator: "|")
+			job.orderings.append(.init(first: Int(ordering[0])!, second: Int(ordering[1])!))
+		}
+		for index in (pageBreak+1) ..< lines.count {
+			let pages = lines[index].split(separator: ",")
+			job.pages.append(pages.map { Int($0)! } )
+		}
+		return job
+	}
+	
 	func solveA() -> String {
 		solveA("Input5").description
 	}
@@ -22,8 +47,31 @@ class Solve5: PuzzleSolver {
 		solveB("Input5").description
 	}
 	
+	func passes(_ orderings: [Ordering], _ pages: [Int]) -> Bool {
+		for index in 0 ..< pages.count {
+			let applicable = orderings.filter { $0.second == pages[index] }
+			for check in (index + 1) ..< pages.count {
+
+				if applicable.contains(where: { $0.first == pages[check] }) {
+					return false
+				}
+			}
+		}
+		return true
+	}
+	
+	func middlePage(pages: [Int]) -> Int {
+		pages[pages.count/2]
+	}
+	
 	func solveA(_ fileName: String) -> Int {
-		0
+		let job = load(fileName)
+		return job.pages.reduce(0) { count, pages in
+			if !passes(job.orderings, pages) {
+				return count
+			}
+			return count + middlePage(pages: pages)
+		}
 	}
 
 	func solveB(_ fileName: String) -> Int {
