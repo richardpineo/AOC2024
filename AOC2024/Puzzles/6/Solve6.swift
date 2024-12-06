d43d688b4e2bb70f0823b8f06e81db30ad4ec827
Richard Pineo <Richard.E.Pineo@ge.com>
2024-12-05 22:52:29 -0800

    remove bearing

--- AOC2024/Puzzles/6/Solve6.swift
+++ AOC2024/Puzzles/6/Solve6.swift
@@ -35,25 +35,6 @@ class Solve6: PuzzleSolver {
 		return positions.count
 	}
 	
-	@DebugDescription
-	struct Bearing: Hashable {
-		var position: Position2D
-		var heading: Heading
-		
-		// Does not change the heading of the original bearing.
-		func offset(_ inDirection: Heading, _ distance: Int = 1) -> Bearing {
-			.init(position: position.offset(inDirection, distance), heading: heading)
-		}
-		
-		func move(_ distance: Int = 1) -> Bearing {
-			.init(position: position.offset(heading, distance), heading: heading)
-		}
-		
-		var debugDescription: String {
-			"Bearing \(heading) at \(position.displayString)"
-		  }
-	}
-	
 	func startingFrom(grid: Grid2D) -> Bearing {
 		let startingPos = grid.allPositions.first {
 			grid.value($0) == "^"
